dc80eaeb45c04afd0f5eb6064db309501888ab5c
NafiuNoman <nafiunoman@gmail.com>
2020-10-29 11:38:29 +0600

    Ongoing

--- app/src/main/java/com/example/internvideoview/MainActivity.java
+++ app/src/main/java/com/example/internvideoview/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.internvideoview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,9 +24,18 @@ public class MainActivity extends AppCompatActivity {
         Uri uri =Uri.parse(videoPath);
         videoView.setVideoURI(uri);
 
+
         MediaController mediaController = new MediaController(this);
-        videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
+
+        videoView.setMediaController(mediaController);
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this, "Video is finished", Toast.LENGTH_SHORT).show();
+            }
+        });
+        videoView.requestFocus();
 
     }
 }
